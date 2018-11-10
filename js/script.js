11ff4c8e28dc9404ffb523a5663a74607fab47ae
Archit Rathore <architrathore1@gmail.com>
2018-11-09 19:57:00 -0700

    Added doughnut chart for summary view

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 let activeYear = '2008';
 
 try{
-let mapViewObj = new mapView(activeYear);
-let timeSlider = new TimeSlider(activeYear, mapViewObj);
+	let mapViewObj = new mapView(activeYear);
+	let timeSlider = new TimeSlider(activeYear, mapViewObj);
 
-timeSlider.drawYearBar();
-timeSlider.showViews(activeYear);
+	timeSlider.drawYearBar();
+	timeSlider.showViews(activeYear);
 
-let statisticsViewObj = new StatisticsView();
+	let statisticsViewObj = new StatisticsView();
+
+	var ctx = document.getElementById("summary");
+	console.log(ctx);
+	var myChart = new Chart(ctx, {
+	    type: 'doughnut',
+	    data: {
+	        labels: ['Arson', 'Larceny', 'Traffic'],
+	        datasets: [{
+	            label: 'Percentage of total crimes',
+	            data: [14, 10, 3]
+	        }],
+			backgroundColor: [
+				   '#AA3939', '#226666', '#7B9F35'
+			   ]
+	    }
+	});
 }
 catch(error){
 	console.log(error);
--- js/statisticsView.js
+++ js/statisticsView.js
@@ -1,22 +1,22 @@
 class StatisticsView {
-	
+
 	constructor () {
-		year_svg = d3.select('#yearly-stats')
+		this.year_svg = d3.select('#yearly-stats')
 						.append('svg')
 						.attr('width', 500)
 						.attr('height', 300);
 
-		month_svg = d3.select('#monthly-stats')
+		this.month_svg = d3.select('#monthly-stats')
 						.append('svg')
 						.attr('width', 500)
 						.attr('height', 300);
 
-		weekly_svg = d3.select('#weekly-stats')
+		this.weekly_svg = d3.select('#weekly-stats')
 						.append('svg')
 						.attr('width', 500)
 						.attr('height', 300);
 
-		daily_svg = d3.select('#daily-stats')
+		this.daily_svg = d3.select('#daily-stats')
 						.append('svg')
 						.attr('width', 500)
 						.attr('height', 300);
