836b40b57262febc9254bacc04d06f997b0a13d9
Yash Gangrade <yashgangrade09@gmail.com>
2018-11-24 12:58:53 -0700

    Updated MapView to support clustering of points

--- Code/js/mapView - Copy.js
+++ Code/js/mapView - Copy.js
@@ -3,14 +3,8 @@ class mapView{
 	constructor (activeYear) {
 		this.activeYear = activeYear;
     	d3.select("#map-view-box").attr("style", "height:500px");
-    	this.mymap = L.map('map-view-box', {
-    		center: [40.759759, -111.861619],
-    		minZoom: 9,
-    		zoom: 10
-    	});
-
-    	// this.LayerGroup = L.layerGroup().addTo(this.mymap);
-    	this.markerClusters = L.markerClusterGroup();
+    	this.mymap = L.map('map-view-box').setView([40.759759, -111.861619], 13);
+    	this.LayerGroup = L.layerGroup().addTo(this.mymap);
 	}
 
 	showCrimeMarkers(markers) {
@@ -44,60 +38,46 @@ class mapView{
 	    var icons = [redIcon, greenIcon, violetIcon];
 
         for(let i = 0; i < markers.length; i++){
-            let myicon;
-            // if(markers[i]["UCR DESCRIPTION"] == "TRAFFIC")
-            //     icon = icons[0];
-            // else if(markers[i]["UCR DESCRIPTION"] == "LARCENY")
-            //     icon = icons[2];
-            // else
-            //     icon = icons[1];
-            myicon = icons[0];
+            let icon;
+            if(markers[i]["UCR DESCRIPTION"] == "TRAFFIC")
+                icon = icons[0];
+            else if(markers[i]["UCR DESCRIPTION"] == "LARCENY")
+                icon = icons[2];
+            else
+                icon = icons[1];
 
-            let markerTemp = L.marker([markers[i]["latitude"], markers[i]["longitude"]], {icon: myicon})
-             // .bindPopup("Location of Crime: " + markers[i]["address"])
-             // .addTo(that.markerClusters);
-             that.markerClusters.addLayer( markerTemp );
+
+            let markerTemp = L.marker([markers[i].LATITUDE, markers[i].LONGITUDE], {icon: icon})
+             .bindPopup("Location of Crime: " + markers[i]["LOCATION"] + "<br>Crime Type: "
+             	+ markers[i]["UCR DESCRIPTION"] + "<br>Date of Occurence: " + markers[i]["OCCUR DATE"])
+             // .addTo(that.LayerGroup);
+             that.LayerGroup.addLayer(markerTemp);
         }
-    }
-
-    showCrimeMarkersCircles(markers){
-    	let that = this;    	
-    	// var div_circle = L.divIcon({ className: "my-div-icon"});
-
-    	for(let i = 0; i < markers.length; i++){
-    		let markerTemp = L.circle([markers[i]["latitude"], markers[i]["longitude"]], {
-    			color: 'red',
-    			fillcolor: 'red',
-    			fillOpacity: 0.4,
-    			radius: 15
-    		})
-    		.bindPopup("Location of Crime: " + markers[i]["address"])
-    		// let markerTemp = L.marker([markers[i]["latitude"], markers[i]["longitude"], {icon: div_circle} );
-    		// that.LayerGroup.addLayer(markerTemp);
-    	}	
     }
 
     showViews(year){
     	let that = this;
 	    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-	    subdomains: ['a', 'b', 'c'],
-	  	maxZoom: 18,
-	    minZoom: 9
+	    maxZoom: 18,
+	    minZoom: 10
 	    }).addTo(that.mymap);
 
+
 	    // var marker = L.marker([40.759759, -111.861619]).addTo(that.mymap);
-	    // this.LayerGroup.clearLayers();
+	    this.LayerGroup.clearLayers();
 
 	    // var marker = L.marker([40.759759, -111.861619]).addTo(that.LayerGroup);
 	    // var marker = L.marker([40.759759, -111.861619]);
 
 	    // that.LayerGroup.addLayer(marker);
 
-    	d3.csv("data/gmap_addresses.csv").then(function(yearData){
+    	d3.csv("dummydata/" + year + ".csv").then(function(yearData){
         	// console.log(JSON.parse(JSON.stringify(yearData)));
         	that.showCrimeMarkers(JSON.parse(JSON.stringify(yearData)));
     	});
-	    that.mymap.addLayer(that.markerClusters);
     }
+
+
+
 }
