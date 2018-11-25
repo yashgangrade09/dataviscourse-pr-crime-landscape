401635c21d22e0938ae65966dc09f4c1f456ee5d
rebeka-mukherjee <rebeka.mukherjee@utah.edu>
2018-11-25 14:37:32 -0700

    Changed statistics view

--- code/js/script.js
+++ code/js/script.js
@@ -10,28 +10,6 @@ try{
 
 	let statisticsViewObj = new StatisticsView();
 
-	var ctx = document.getElementById("summary");
-	var myChart = new Chart(ctx, {
-	    type: 'doughnut',
-	    data: {
-	        labels: ["Traffic", "Larceny", "Arson"],
-	        datasets: [{
-	            label: '# of Votes',
-	            data: [12, 19, 3],
-	            backgroundColor: [
-	                '#AA3939',
-	                '#226666',
-	                '#7B9F35'
-	            ],
-	            borderColor: [
-	                '#000000',
-	                '#000000',
-	                '#000000'
-	            ],
-	            borderWidth: 1
-	        }]
-	    }	    
-	});
 
 
 	var yearChart = new Chart (document.getElementById("year-chart") , {
@@ -64,66 +42,6 @@ try{
 		}
 	});
 
-	var monthChart = new Chart (document.getElementById("month-chart") , {
-		type: 'line',
-		data: {
-			labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-			datasets: [{
-				data: [12, 13, 6, 5, 8, 9, 17, 11, 19, 10, 5, 25],
-				label: 'Traffic',
-				borderColor: '#AA3939',
-				fill: false
-			},
-			{
-				data: [8, 7, 9, 3, 4, 6, 5, 7, 1, 2, 3, 4],
-				label: 'Larceny',
-				borderColor: '#226666',
-				fill: false
-			},
-			{
-				data: [1, 0, 0, 1, 2, 2, 1, 0, 0, 0, 3, 4],
-				label: 'Arson',
-				borderColor: '#7B9F35',
-				fill: false
-			}]},
-		options: {
-			title: {
-				display: true,
-				text: 'Monthly Statistics'
-			}
-		}
-	});
-
-	var weeklyChart = new Chart (document.getElementById("week-chart") , {
-		type: 'line',
-		data: {
-			labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-			datasets: [{
-				data: [12, 13, 6, 5, 8, 9, 17],
-				label: 'Traffic',
-				borderColor: '#AA3939',
-				fill: false
-			},
-			{
-				data: [6, 5, 7, 1, 2, 3, 4],
-				label: 'Larceny',
-				borderColor: '#226666',
-				fill: false
-			},
-			{
-				data: [1, 0, 0, 0, 0, 3, 4],
-				label: 'Arson',
-				borderColor: '#7B9F35',
-				fill: false
-			}]},
-		options: {
-			title: {
-				display: true,
-				text: 'Weekly Statistics'
-			}
-		}
-	});
-
 	var dailyChart = new Chart (document.getElementById("day-chart") , {
 		type: 'line',
 		data: {
@@ -149,7 +67,7 @@ try{
 		options: {
 			title: {
 				display: true,
-				text: 'Hourly Statistics'
+				text: 'Daily Statistics'
 			}
 		}
 	});
--- code/js/statisticsView.js
+++ code/js/statisticsView.js
@@ -2,26 +2,127 @@ class StatisticsView {
 
 	constructor () {
 
-		/*
-		this.year_svg = d3.select('#yearly-stats')
-						.append('svg')
-						.attr('width', 500)
-						.attr('height', 300);
+		let crime_list = ['TRAFFIC', 'LARCENY', 'ARSON'];
 
-		this.month_svg = d3.select('#monthly-stats')
-						.append('svg')
-						.attr('width', 500)
-						.attr('height', 300);
+		d3.csv("data/2008_processed.csv").then(data => {
+			let crime0 = 0;
+			let crime1 = 0;
+			let crime2 = 0;
 
-		this.weekly_svg = d3.select('#weekly-stats')
-						.append('svg')
-						.attr('width', 500)
-						.attr('height', 300);
+            let crime0_month = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            let crime1_month = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            let crime2_month = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-		this.daily_svg = d3.select('#daily-stats')
-						.append('svg')
-						.attr('width', 500)
-						.attr('height', 300);
-		*/
+            let crime0_week = [0, 0, 0, 0, 0, 0, 0];
+            let crime1_week = [0, 0, 0, 0, 0, 0, 0];
+            let crime2_week = [0, 0, 0, 0, 0, 0, 0];
+
+            let crime0_day = [0, 0, 0, 0, 0, 0, 0, 0];
+            let crime1_day = [0, 0, 0, 0, 0, 0, 0, 0];
+            let crime2_day = [0, 0, 0, 0, 0, 0, 0, 0];
+
+            for (let i=0; i<data.length; i++) {
+            	if (data[i]['DESCRIPTION'] == crime_list[0]) {
+            		crime0 += 1
+            		crime0_month[data[i]['MONTH']-1] += 1;
+            		crime0_week[data[i]['TIME']-1] += 1;
+            	}
+            	else if (data[i]['DESCRIPTION'] == crime_list[1]) {
+            		crime1 += 1
+            		crime1_month[data[i]['MONTH']-1] += 1;
+            		crime1_week[data[i]['TIME']-1] += 1;
+            	}
+            	else if (data[i]['DESCRIPTION'] == crime_list[2]) {
+            		crime2 += 1
+            		crime2_month[data[i]['MONTH']-1] += 1;
+            		crime2_week[data[i]['TIME']-1] += 1;
+            	}
+            }
+
+            var summaryChart = new Chart (document.getElementById("summary"), {
+            	type: 'doughnut',
+            	data: {
+            		labels: crime_list,
+            		datasets: [{
+            			label: "Number of incidents.",
+            			data: [crime0, crime1, crime2],
+            			backgroundColor: [
+	                '#AA3939',
+	                '#226666',
+	                '#7B9F35'
+	            ],
+	            borderColor: [
+	                '#000000',
+	                '#000000',
+	                '#000000'
+	            ],
+	            borderWidth: 1
+            		}]
+            	}
+            });
+
+            var monthChart = new Chart (document.getElementById("month-chart") , {
+				type: 'line',
+				data: {
+					labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+					datasets: [{
+						data: crime0_month,
+						label: crime_list[0],
+						borderColor: '#AA3939',
+						fill: false
+					},
+					{
+						data: crime1_month,
+						label: crime_list[1],
+						borderColor: '#226666',
+						fill: false
+					},
+					{
+						data: crime2_month,
+						label: crime_list[2],
+						borderColor: '#7B9F35',
+						fill: false
+					}]},
+				options: {
+					title: {
+						display: true,
+						text: 'Monthly Statistics'
+					}
+				}
+			});
+
+			var weeklyChart = new Chart (document.getElementById("week-chart") , {
+				type: 'line',
+				data: {
+					labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+					datasets: [{
+						data: crime0_week,
+						label: crime_list[0],
+						borderColor: '#AA3939',
+						fill: false
+					},
+					{
+						data: crime1_week,
+						label: crime_list[1],
+						borderColor: '#226666',
+						fill: false
+					},
+					{
+						data: crime2_week,
+						label: crime_list[2],
+						borderColor: '#7B9F35',
+						fill: false
+					}]},
+				options: {
+					title: {
+						display: true,
+						text: 'Weekly Statistics'
+					}
+				}
+			});
+
+
+
+		});
 	}
 }
