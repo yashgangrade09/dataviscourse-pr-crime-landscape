85e40e52f2b43134deee78e001bd552e48392ad9
rebeka-mukherjee <rebeka.mukherjee@utah.edu>
2018-11-30 04:44:33 -0700

    Fixed selection view coordination with statistics view

--- code/js/selectionView.js
+++ code/js/selectionView.js
@@ -1,17 +1,12 @@
 function getData(mapViewObj, statisticsViewObj, timeSliderObj, year){
-	console.log("In get data ", mapViewObj);
+	//console.log("In get data ", mapViewObj);
 	let checkboxes = document.getElementsByName("crimeType");
-	// let checkboxes = document.getElementsByClassName("checkboxes");
-	// console.log(checkboxes);
 	let crime_list = [];
-
 	for (var i = 0; i < checkboxes.length; i++) {
 		if (checkboxes[i].type == 'checkbox' && checkboxes[i].checked == true) {
 			crime_list.push(checkboxes[i].value);
 		}
-	}
-	
+	}	
 	mapViewObj.showViews(year, crime_list);
-	statisticsViewObj.showViews(activeYear, crime_list);
-	timeSliderObj.showViews(activeYear);
+	statisticsViewObj.showViews(year, crime_list);
 }
